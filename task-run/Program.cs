5c71071c4cdd19445705d3571656d883239025a0
kjiak <kjiakai@gmail.com>
2021-11-15 13:59:40 +0800

    initial commit

---
+++ task-run/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
